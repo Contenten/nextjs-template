506ff54b24cd1105dea05785811daff2a75b0f60
wcw <344078971@qq.com>
2025-02-05 22:13:49 +0800

    refactor: enhance next.config.js for platform-specific configurations and image handling

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
 import { createContentlayerPlugin } from "next-contentlayer2"
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      "/blocks/*": ["./registry/**/*"],
-    },
-  },
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
+const nextConfig = resolveConfig(process.env.PLATFORM)
+
+/**
+ * 
+ * @param {string} platform 
+ */
+function resolveConfig(platform) {
+  let images = {
     remotePatterns: [
       {
         protocol: "https",
@@ -20,56 +18,71 @@ const nextConfig = {
         hostname: "images.unsplash.com",
       },
     ],
-  },
-  redirects() {
-    return [
-      {
-        source: "/components",
-        destination: "/docs/components/accordion",
-        permanent: true,
+  }
+
+  let output = undefined
+
+  let basePath = undefined
+
+  // ssg
+  if (platform === "github pages" || platform === "cloudflare pages") {
+    images = {
+      unoptimized: true,
+    }
+    output = "export"
+  }
+
+  if (platform === 'github pages') {
+    basePath = process.env.NODE_ENV === "production" ? `/${pkg.name}` : undefined;
+  }
+
+  if (platform === 'vps') {
+    output = 'standalone'
+  }
+
+  return ({
+    reactStrictMode: true,
+    experimental: {
+      typedRoutes: true,
+      optimizePackageImports: ["@radix-ui/react-icons"],
+    },
+    images,
+    headers: async () => {
+      return [
+        {
+          source: "/(.*)",
+          headers: [
+            {
+              key: "X-Content-Type-Options",
+              value: "nosniff",
+            },
+            {
+              key: "X-Frame-Options",
+              value: "SAMEORIGIN",
+            },
+            {
+              key: "X-XSS-Protection",
+              value: "1; mode=block",
+            },
+            {
+              key: "Strict-Transport-Security",
+              value: "max-age=63072000; includeSubDomains; preload",
+            },
+          ],
+        },
+      ];
+    },
+    logging: {
+      fetches: {
+        fullUrl: true,
       },
-      {
-        source: "/docs/components",
-        destination: "/docs/components/accordion",
-        permanent: true,
-      },
-      {
-        source: "/examples",
-        destination: "/examples/mail",
-        permanent: false,
-      },
-      {
-        source: "/docs/primitives/:path*",
-        destination: "/docs/components/:path*",
-        permanent: true,
-      },
-      {
-        source: "/figma",
-        destination: "/docs/figma",
-        permanent: true,
-      },
-      {
-        source: "/docs/forms",
-        destination: "/docs/components/form",
-        permanent: false,
-      },
-      {
-        source: "/docs/forms/react-hook-form",
-        destination: "/docs/components/form",
-        permanent: false,
-      },
-      {
-        source: "/sidebar",
-        destination: "/docs/components/sidebar",
-        permanent: true,
-      },
-      {
-        source: "/react-19",
-        destination: "/docs/react-19",
-        permanent: true,
-      },
-    ]
-  },
+    },
+    typescript: {
+      ignoreBuildErrors: true
+    },
+    output,
+    basePath,
+  }) 
 }
 
 const withContentlayer = createContentlayerPlugin({
